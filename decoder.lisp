b907d9499e8974324c0f9dcaceeedb0c45c8158e
oklaridentitet <oklaridentitet@gmail.com>
2015-04-08 11:52:45 +0200

    use relative paths

--- decoder.lisp
+++ decoder.lisp
@@ -1,13 +1,13 @@
-;http://www.gigamonkeys.com/book/practical-parsing-binary-files.html
+4;http://www.gigamonkeys.com/book/practical-parsing-binary-files.html
 ;http://golang-challenge.com/go-challenge1/
 ;http://www.lispworks.com/documentation/lw51/CLHS/Body/f_rd_seq.htm
 ;http://www.lispworks.com/documentation/lw60/CLHS/Body/f_rd_by.htm
 
-(with-open-file (s "/Users/jite/code/golang-challenge-1/fixtures/pattern_1.splice" :element-type 'unsigned-byte)
+(with-open-file (s "fixtures/pattern_1.splice" :element-type 'unsigned-byte)
   (format t "~S ~S" (read-byte s) (read-byte s nil 'eof)))
 
 ; First 6 bytes should be read.o
-(READ-NULL-TERMINATED-ASCII (open "/Users/jite/code/golang-challenge-1/fixtures/pattern_1.splice" :element-type '(unsigned-byte 8)))
+(READ-NULL-TERMINATED-ASCII (open "fixtures/pattern_1.splice" :element-type '(unsigned-byte 8)))
 
 (defconstant +null+ (code-char 0))
 
